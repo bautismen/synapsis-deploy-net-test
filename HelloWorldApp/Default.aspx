e5cd3e1ed7e2df9d181abf5788392fdbfa0fa9ed
bautismen <bautismen@gmail.com>
2026-08-26 14:03:17 -0600

    cambios5

--- HelloWorldApp/Default.aspx
+++ HelloWorldApp/Default.aspx
@@ -8,7 +8,7 @@
 <body style="font-family: Segoe UI, Arial, sans-serif; text-align: center; margin-top: 80px;">
     <form id="form1" runat="server">
         <div>
-            <h1>Hola Mundo 👋</h1>
+            <h1>Hola Mundo Synapsis!</h1>
             <p>ASP.NET Framework 4.8.1 corriendo en IIS</p>
             <p>Servidor: <asp:Literal ID="litServerName" runat="server" /></p>
             <p>Hora del servidor: <asp:Literal ID="litServerTime" runat="server" /></p>
--- HelloWorldApp/Default.aspx.designer.cs
+++ HelloWorldApp/Default.aspx.designer.cs
@@ -1,9 +1,53 @@
+//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
 namespace HelloWorldApp
 {
+
+
     public partial class Default
     {
+
+        /// <summary>
+        /// Control form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Control litServerName.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
         protected global::System.Web.UI.WebControls.Literal litServerName;
+
+        /// <summary>
+        /// Control litServerTime.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
         protected global::System.Web.UI.WebControls.Literal litServerTime;
+
+        /// <summary>
+        /// Control litBuildTag.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
         protected global::System.Web.UI.WebControls.Literal litBuildTag;
     }
 }
